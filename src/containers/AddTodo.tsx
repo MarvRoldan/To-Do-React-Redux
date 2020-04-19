8f861565e9fad031f9116e54804ec02125c210b5
MarvRoldan <marvinroldan_@hotmail.com>
2020-04-19 12:44:21 -0600

    Added containers

---
+++ src/containers/AddTodo.tsx
@@ -0,0 +1,8 @@
+import React, { Fragment } from "react";
+import { connect } from "react-redux";
+
+const AddTodo = () => {
+  return <Fragment></Fragment>;
+};
+
+export default connect()(AddTodo);
