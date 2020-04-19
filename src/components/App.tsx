a78cb284ccb3d249c46303d9d386c049763af7b6
MarvRoldan <marvinroldan_@hotmail.com>
2020-04-19 12:29:01 -0600

    Starting components

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+/*import AddTodo from '../containers/AddTodo';
+import VisibleTodoList from '../containers/VisibleTodoList';*/
+
+const App = () => (
+    <Fragment>
+        
+    </Fragment>
+);
+
+export default App;
